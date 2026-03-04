a233b9d3ccbfeaa6cc0e20f3646ffc8656c42a08
mtag1337 <mtag1337@gmail.com>
2026-03-04 04:02:45 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
+import os
+import sys
+
+# السطر ده "إجباري" لـ Vercel عشان يشوف مجلد academy اللي جنبه
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from academy import create_app
 
-# Vercel بيدور على متغير اسمه app أو application بره أي Condition
 app = create_app()
-
-# السطر ده اختياري بس بيساعد في حل المشاكل لو حصلت
-application = app 
+application = app
 
 if __name__ == '__main__':
-    # ده هيفضل شغال لو جربت تشغل الملف من جهازك
-    app.run(debug=True)
+    app.run()
